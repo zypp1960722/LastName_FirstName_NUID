ea5b53d7617c5d1fee21e94426446a803d660c3e
zypp1960722 <zhang.ruiz@husky.neu.edu>
2020-03-29 20:52:29 -0400

    food delivery

---
+++ db4odemo Peter/db4odemo/src/Business/Restaurant/OrderItem.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Restaurant;
+
+/**
+ *
+ * @author liluyi
+ */
+public class OrderItem {
+    private MenuItem dishAndPrice;
+    private int num;
+    
+    public OrderItem()
+    {
+        
+    }
+
+    public OrderItem(MenuItem dishAndPrice, int num) {
+        this.dishAndPrice = dishAndPrice;
+        this.num = num;
+    }
+
+    public MenuItem getDishAndPrice() {
+        return dishAndPrice;
+    }
+
+    public void setDishAndPrice(MenuItem dishAndPrice) {
+        this.dishAndPrice = dishAndPrice;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+    @Override
+    public String toString()
+    {
+        return this.dishAndPrice.getDishName();
+    }
+    
+}
